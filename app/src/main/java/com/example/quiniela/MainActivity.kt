169fa7bdfa70c3edce5d84758bd563dbc8bbeea5
Grifo55 <guillepcorbella@hotmail.com>
2022-06-14 21:22:36 +0200

    Now the number of games can be chiised and it's limited to 15

--- app/src/main/java/com/example/quiniela/MainActivity.kt
+++ app/src/main/java/com/example/quiniela/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.TextView
 import android.widget.Toast
 import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,11 +14,20 @@ class MainActivity : AppCompatActivity() {
 
         val resultadoenpantalla: TextView = findViewById(R.id.texto_Resultado)
         val botonPantalla: Button = findViewById(R.id.button_calcularJugada)
+        val numPartidos: EditText = findViewById(R.id.editTextNumPartidos)
+
+
         botonPantalla.setOnClickListener{
-            val notificacionCorta = Toast.makeText(this,"Calculando jugada",Toast.LENGTH_SHORT)
-            notificacionCorta.show()
+            var total = numPartidos.text.toString().toInt()
+            if (total > 15){
+                val notificacionerror = Toast.makeText(this,"El máximo de partidos es 15",Toast.LENGTH_LONG)
+                notificacionerror.show()
+                total = 15
+            }
+            //val notificacionCorta = Toast.makeText(this,"Calculando jugada",Toast.LENGTH_SHORT)
+            //notificacionCorta.show()
             var listaResultados = ""
-            for (i in 1..14){
+            for (i in 1..total){
                 val aleatorio = (1..100).random()
                 if (aleatorio<50){
                     listaResultados = listaResultados + "1\n"
